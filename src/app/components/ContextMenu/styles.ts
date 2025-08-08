b2b49d7feb11e7813c99c1e6fe450f7a0f9c2d77
Katherine Negrete <katherine.negrete@drsecurity.net>
2025-08-08 14:02:18 -0600

    New features in tables
    Why is the change necessary?
    - Dynamic calendar and dynamic pagination have been added to tables
    
    Where were the changes made?
    - DataTables.tsx
    
    Does it affect other systems?
    - No, it does not affect

--- src/app/components/ContextMenu/styles.ts
+++ src/app/components/ContextMenu/styles.ts
@@ -3,7 +3,7 @@ export const contextMenuStyles = {
   Trigger: 'inline-block',
   Menu: 'absolute mt-2 w-48 rounded-md shadow-300 bg-white-100 z-50 p-1',
   ItemBase:
-    'w-full flex justify-between items-center px-4 py-2 text-left font-normal text-b1 rounded-sm',
+    'w-full flex justify-between items-center px-4 py-2 text-left text-gray-70 font-normal text-b1 rounded-sm',
   ItemPressed: 'bg-green-10 text-black-100',
   ItemDisabled: 'text-gray-40 cursor-not-allowed',
   ItemDanger: 'text-alert-red-100',
