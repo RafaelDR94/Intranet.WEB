7e84431266fbf4ccb3f1ec40937bfdb76e365304
Katherine Negrete <katherine.negrete@drsecurity.net>
2025-09-25 18:04:41 -0600

    Fix in loop when making a request to firebase
    Why is this change necessary?
    - To avoid making infinite requests to firebase
    
    Where were the changes made?
    - useVoucherPink
    - useVoucherBlue
    
    Does it affect other systems?
    - No, it does not

--- src/app/components/DynamicForm/utilities/getValidationSchema.ts
+++ src/app/components/DynamicForm/utilities/getValidationSchema.ts
@@ -32,7 +32,7 @@ export const getValidationSchema = (fields: FieldModel[]) => {
         });
         acc[field.name] = schema;
       } else if (field.type === 'file') {
-        let schema = Yup.mixed().nullable(true);
+        let schema = Yup.mixed().nullable();
         field.validations?.forEach((rule) => {
           if (rule.type === 'required') {
             schema = schema.required('Este campo es requerido');
