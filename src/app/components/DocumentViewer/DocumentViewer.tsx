4f6e8195775e1941a232625a941e7617c341f1b3
Katherine Negrete <katherine.negrete@drsecurity.net>
2025-10-30 12:02:22 -0600

    Documents viewer component

---
+++ src/app/components/DocumentViewer/DocumentViewer.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import CancelIcon from "@/assets/icons/acciones/cancel.svg";
+import DownloadIcon from "@/assets/icons/acciones/download.svg";
+interface DocumentViewerProps {
+  fileUrl: string;
+  title?: string;
+  onClose: () => void;
+}
+
+const DocumentViewer: React.FC<DocumentViewerProps> = ({ fileUrl, title, onClose }) => {
+        console.log('fileUrl', fileUrl);
+
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center backdrop-blur-[2px] bg-[rgba(0,42,65,0.70)]">
+      <div className="">
+        {/* Header */}
+        <div className="flex justify-between items-center pl-4">
+          <h2 className="text-white text-s1">
+            {title || "Vista previa del documento"}
+          </h2>
+          <div className="flex gap-3 items-center">
+            <a
+              href={fileUrl}
+              download
+              className="text-gray-300 hover:text-white transition"
+              title="Descargar"
+            >
+            <DownloadIcon />
+            </a>
+            <button
+              onClick={onClose}
+              className="text-gray-300 hover:text-white transition text-xl"
+            >
+              <CancelIcon />
+            </button>
+          </div>
+        </div>        
+
+        {/* Document preview */}
+        <div className="p-4 flex justify-center">
+          <iframe
+            src={fileUrl}
+            className="w-[600px] h-[80vh] rounded-lg"
+            title={title}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DocumentViewer;
